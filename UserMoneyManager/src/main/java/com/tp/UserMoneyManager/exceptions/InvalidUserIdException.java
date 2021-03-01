e878ff04ea4739df2b3583f9738dc6e9f4c5dfaa
YesratRahman <yr2818@vt.edu>
2021-03-01 14:47:55 -0500

    Added money manager frontend

---
+++ UserMoneyManager/src/main/java/com/tp/UserMoneyManager/exceptions/InvalidUserIdException.java
@@ -0,0 +1,10 @@
+package com.tp.UserMoneyManager.exceptions;
+
+public class InvalidUserIdException extends Exception{
+    public InvalidUserIdException(String message){
+        super(message);
+    }
+    public InvalidUserIdException(String message, Throwable innerException){
+        super(message, innerException);
+    }
+}
